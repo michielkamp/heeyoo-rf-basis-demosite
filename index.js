fd4fdb3ceb4030357567424a12420240aa1c4517
mytzenka <39919027+mytzenka@users.noreply.github.com>
2024-04-14 16:49:37 +0200

    add more features

---
+++ index.js
@@ -0,0 +1,41 @@
+function showAlertBox() {
+    alert("Hallo!");
+    document.getElementById("slowToEnable").disabled = true;
+    waitToEnable();
+}
+
+function showInputBox() {
+    const myName = prompt("Wat is je naam?");
+    if (myName != null) {
+        document.getElementById("showMyName").innerHTML =
+            "Hallo " + myName + "!";
+    }
+}
+
+function waitToEnable() {
+    const randomTime = (Math.random() * 10000) + 2000;
+    setTimeout(function() {
+        console.log("Waited for " + randomTime + " milliseconds.");
+        document.getElementById("slowToEnable").disabled = false;
+    }, randomTime);
+}
+
+function percentChance(chance) {
+    if (Math.random() <= chance)
+        return true;
+    else
+        return false;
+}
+
+function startFlakyProcess() {
+    if (percentChance(0.3)) {
+        console.log("Process will start soon...")
+        setTimeout(function() {
+            window.open("processStarted.html", "_self");
+        }, 3000);
+    } else {
+        console.log('FAIL: process will not start');
+    }
+}
+
+waitToEnable(10000);
